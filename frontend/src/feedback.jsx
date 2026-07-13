fdc05f2152d1d661d73f960059d1cd0bd5e628ff
parthosaha58 <parthosaha032@gmail.com>
2026-07-14 04:11:59 +0600

    Fix import path for Feedback.css

--- frontend/src/feedback.jsx
+++ frontend/src/feedback.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import Post_structure from "./components/post_structure";
-import "./Feedback.css";
+import ".feedback.css";
 
 const FeedbackPage = () => {
   const [feedback, setFeedback] = useState("");
